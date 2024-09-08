cdf913aa781c7a579157ca807c439a6f52224737
MarryAngel <maria.kruger.miranda@gmail.com>
2024-09-08 16:52:36 -0300

    new rooms and new world

--- Livre/mundo.py
+++ Livre/mundo.py
@@ -6,7 +6,7 @@ from programador import Programador
 
 class Mundo():
     def __init__(self) -> None:
-        self.mapaOriginal, self.mapaAtual = self.carregarSala("salas/mundo0.txt")
+        self.mapaOriginal, self.mapaAtual = self.carregarSala("salas/mundo.txt")
         self.salas = self.criarSalas()
         self.salaAtual = self.salas[10][6]
         self.camera = Camera(self, (250,250))
@@ -48,7 +48,7 @@ class Mundo():
         for i in range(len(self.mapaAtual)):
             salas.append([])
             for j in range(len(self.mapaAtual[i])):
-                salas[-1].append(Sala(random.randint(0,0), self.mapaOriginal[i][j], self, (i,j))) # sorteia de 0 a 9
+                salas[-1].append(Sala(random.randint(1,10), self.mapaOriginal[i][j], self, (i,j))) # sorteia de 0 a 9
         return salas
 
     def carregarSala(self, arquivo):
